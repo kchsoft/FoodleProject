4167220aae11646b2dc0d96409a1d04ff7809f42
kchranger <kchranger.ajou.ac.kr>
2023-07-10 11:40:09 +0900

    User Dao , Service Test

---
+++ src/test/java/com/foodle/app/Service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.foodle.app.Service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class UserServiceTest {
+
+    @Test
+    public void userCheck() {
+    }
+}
